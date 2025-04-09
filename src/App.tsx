f55f28de35e1894cf0dd11c26163a68cad508ad3
vinimaffra03 <viniciushmdem15@gmail.com>
2025-04-08 21:40:50 -0300

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import BasicExample from "./components/calculator"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        {/* header */}
+        {/* criar cabecalho com icone e barra de links para apps de financas */}
+        <BasicExample />
+
+
+
+
+        {/* calculator */}
+
+
+
+
+
+
+        {/* aside */}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/calculator/index.tsx
@@ -0,0 +1,60 @@
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import granacrescelogo from "../../assets/granacrescelogo.png";
+import "./styles.css";
+
+function BasicExample() {
+  return (
+    // Navbar principal da aplicação
+    <Navbar expand="lg" className="bg-body-tertiary p-0">
+      <Container fluid className="px-0">
+        <img
+          // logo principal da navbar
+          src={granacrescelogo}
+          alt="Logo"
+          className="img-fluid align-top mx-4"
+          style={{ maxHeight: "5em", width: "5em" }}
+        />
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Notícias</Nav.Link>
+            <NavDropdown title="Mercados" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                <a
+                  href="https://www.coingecko.com/pt"
+                  className="no-bootstrap-link"
+                >
+                  Criptoativos / DeFi
+                </a>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                <a href="https://br.tradingview.com/symbols/NYSE-NYA/">
+                  New York Stock Exchange (NYSE) - EUA
+                </a>
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                <a href="https://br.tradingview.com/symbols/NASDAQ-IXIC/">
+                  Nasdaq Stock Market - EUA
+                </a>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                <a href="https://br.tradingview.com/chart/?symbol=BMFBOVESPA%3AIBOV">
+                  Ibovespa (B3) - Brasil
+                </a>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
